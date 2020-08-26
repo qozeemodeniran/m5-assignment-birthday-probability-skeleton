288d7497973114d87598f5ed6b191d7c76c33a3f
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-08-26 14:54:30 +0100

    created an app that check for people with the same birthday froma group

---
+++ app/src/main/java/mooc/vandy/java4android/birthdayprob/logic/Logic.java
@@ -0,0 +1,108 @@
+package mooc.vandy.java4android.birthdayprob.logic;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import mooc.vandy.java4android.birthdayprob.ui.OutputInterface;
+
+/**
+ * This is where the logic of this App is centralized for this assignment.
+ * <p>
+ * The assignments are designed this way to simplify your early Android interactions.
+ * Designing the assignments this way allows you to first learn key 'Java' features without
+ * having to beforehand learn the complexities of Android.
+ *
+ */
+public class Logic 
+       implements LogicInterface {
+    /**
+     * This is a String to be used in Logging (if/when you decide you
+     * need it for debugging).
+     */
+    public static final String TAG =
+        Logic.class.getName();
+
+    /**
+     * This is the variable that stores our OutputInterface instance.
+     * <p>
+     * This is how we will interact with the User Interface
+     * [MainActivity.java].
+     * <p>
+     * It is called 'mOut' because it is where we 'out-put' our
+     * results. (It is also the 'in-put' from where we get values
+     * from, but it only needs 1 name, and 'mOut' is good enough).
+    */
+    OutputInterface mOut;
+
+    /**
+     * This is the constructor of this class.
+     * <p>
+     * It assigns the passed in [MainActivity] instance
+     * (which implements [OutputInterface]) to 'out'
+     */
+    public Logic(OutputInterface out){
+        mOut = out;
+    }
+
+    /**
+     * This is the method that will (eventually) get called when the
+     * on-screen button labelled 'Process...' is pressed.
+     */
+    public void process() {
+        int groupSize = mOut.getSize();
+        int simulationCount = mOut.getCount();
+
+        if (groupSize < 2 || groupSize > 365) {
+            mOut.makeAlertToast("Group Size must be in the range 2-365.");
+            return;
+        }
+        if (simulationCount <= 0) {
+            mOut.makeAlertToast("Simulation Count must be positive.");
+            return;
+        }
+
+        double percent = calculate(groupSize, simulationCount);
+
+        // report results
+        mOut.println("For a group of " + groupSize + " people, the percentage");
+        mOut.println("of times that two people share the same birthday is");
+        mOut.println(String.format("%.2f%% of the time.", percent));
+
+    }
+
+    /**
+     * This is the method that actually does the calculations.
+     * <p>
+     * We provide you this method that way we can test it with unit testing.
+     */
+    public double calculate(int size, int count) {
+        // TODO -- add your code here
+//        Variable to store number pair birthday on the same day
+        int same_birthday_count = 0;
+
+//        Running the simulation for a specific number of times
+        for (int i = 0; i < count; i++){
+//            Creating array which corresponds to each day of the year
+            int[] day_of_the_year = new int[365];
+/*            Creating a new random object for each simulation run
+              and seeding the random object with the number of run
+ */
+            Random rand = new Random(i);
+
+            for (int j = 0; j < size; j++){ //Generating a random birthday for each person
+                int birthday = rand.nextInt(365); //Generating random integer in range(0-364)
+                if (day_of_the_year[birthday] == 0){ //No person with same birthday yet
+                    day_of_the_year[birthday] =1;
+                }else { //A pair birthday on same day
+                    same_birthday_count++;
+                    break; //End current simulation and proceed to next simulation
+                }
+            }
+        }
+//        Returning the % of simulations with a pair birthday on same day
+        return ((double) same_birthday_count *100)/ (double) count;
+    }
+
+    // TODO - add your code here
+}
